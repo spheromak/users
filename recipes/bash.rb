16535d126fc92b02e64091141e56fc30b164aae3
Jesse Nelson <spheromak@gmail.com>
2012-10-12 12:14:26 -0700

    brings in compat with ubuntu and redhat. * skel files * dircolors * default shel config * consistant login.defs * consistant behavior on cent/ubuntu

--- recipes/bash.rb
+++ recipes/bash.rb
@@ -10,5 +10,34 @@ cookbook_file "/etc/DIR_COLORS" do
   group "root"
   mode  0644
 end
-# dircolors
 
+cookbook_file "/etc/skel/.bashrc" do 
+  source "skel-bashrc"
+  owner "root"
+  group "root"
+  mode 0644
+end
+
+cookbook_file "/etc/skel/.bash_profile" do 
+  source "skel-bash_profile"
+  owner "root"
+  group "root"
+  mode 0644
+end
+
+cookbook_file "/etc/skel/.bash_logout" do 
+  source "skel-bash_logout"
+  owner "root"
+  group "root"
+  mode 0644
+end
+
+# we touch bash_profile for root so that we ignore the ubuntu bashrc placed there
+# by default
+file "/root/.bash_proflie" do
+  action :create_if_missing
+  content "# overides ~/bashrc on ubuntu"
+  owner "root"
+  group "root"
+  mode 0644
+end

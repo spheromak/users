428eb47fe9c98c624fd316414ec93d1c9618cd9a
Jesse Nelson <spheromak@gmail.com>
2013-03-06 23:13:21 +0900

    bump version

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-maintainer        "Jason K Jackson"
-maintainer_email  "jasonjackson@gmail.com"
+maintainer        "Jesse Nelson"
+maintainer_email  "spheromak@gmail.com"
 license           "All Rights Reserved"
 description       "Creates users and groups from databags and attribs"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version           "1.0.1"
+version           "1.1.0"
 
 depends "helpers"
 depends "vim"

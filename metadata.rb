b9664b14636dd80b49dc219959fb910ac73ae239
Jesse Nelson <spheromak@gmail.com>
2013-03-20 00:21:50 -0700

    udpate metadata doc

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Jesse Nelson"
 maintainer_email  "spheromak@gmail.com"
 license           "All Rights Reserved"
 description       "Creates users and groups from databags and attribs"
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.1.1"
 
 depends "helpers"

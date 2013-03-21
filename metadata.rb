ccd974828f3f28f4d79371cb77d82cc3082ba824
Jesse Nelson <spheromak@gmail.com>
2013-03-21 02:17:33 -0700

    update metadata for a weak dep on the users/vim/git additional cooks

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,10 @@ maintainer_email  "spheromak@gmail.com"
 license           "Apache 2"
 description       "Creates users and groups from databags and attribs"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.1.1"
+version           "1.1.2"
 
 depends "helpers"
-depends "vim"
-depends "ktc-vim"
+
+suggests "ktc-vim"
+suggests "ktc-git"
+suggests "ktc-ssh"

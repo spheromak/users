299c1aa802cf2f01edd16a130887306008f6a144
Jesse Nelson <spheromak@gmail.com>
2013-10-04 11:22:30 -0700

    bump version in dev

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "spheromak@gmail.com"
 license           "Apache 2"
 description       "Creates users and groups from databags and attribs"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.1.5"
+version           "1.1.6"
 
 depends "helpers"
 depends "ktc-vim"

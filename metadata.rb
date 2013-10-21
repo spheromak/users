5310e08030578b0799285c4ab7977a31403ce7e0
ktc-jenkins <jenkins.ktcloudware@gmail.com>
2013-10-21 16:46:06 +0900

    auto_version to 1.1.8

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "spheromak@gmail.com"
 license           "Apache 2"
 description       "Creates users and groups from databags and attribs"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "2.0.1"
+version '1.1.8'
 
 suggests "ktc-git"
 suggests "ktc-ssh"
